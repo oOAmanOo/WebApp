9576120aee28b1ce1525e6efc1a4418c4da0c48d
oOAmanOo <am021024@cm.nsysu.edu.tw>
2024-05-22 17:11:30 +0800

    zh font Noto

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        <h1>Books</h1>
+    <div class="container justify-content-center">
+        <div class="row zh">
+            <img class="col-lg-3" src="../assets/logo.png" alt="Vue logo">
+            <div class="col-lg-5 text-md-start">
+                <h3>Books</h3>
+                <h5>繪本簡介</h5>
+                <p class="alert alert-dismissible alert-light">
+                    繪本ru3-fjfhoshguhdsouaghreghaudshhgwrifjdfuijfdigbuijg</p>
+                <h5 class=" zh">價格:{{}}</h5>
+                <h5 class=" zh">作者:{{}}</h5>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +19,9 @@
 </script>
 
 <style scoped>
+.inline-block {
+    display: inline-flex;
+    margin: 10px;
+}
 
 </style>
