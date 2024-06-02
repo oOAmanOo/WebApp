9d6889a93596495243344e7736d1f4cb8f0c1f5f
oOAmanOo <iamamanda021024@gmail.com>
2024-06-02 14:42:02 +0800

    add login alert

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useCookies } from 'vue3-cookies';
-import IndexPage from "@/views/IndexPage.vue";
 import BooksView from "@/views/BooksView.vue";
 import LoginView from "@/views/LoginView.vue";
 import SignupView from "@/views/SignupView.vue";
 import PurchaseList from "@/views/PurchaseList.vue";
 import Home from "@/views/Home.vue";
 import Cart from "@/views/Cart.vue";
-import test from "@/views/test.vue";
 import UserView from "@/views/UserView.vue";
+import Swal from "sweetalert2";
 
 const { cookies } = useCookies();
 const routes = [
@@ -26,11 +25,6 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: SignupView
-  },
-  {
-    path: '/test',
-    name: 'test',
-    component: test
   },
   {
     path: '/purchase',
@@ -63,8 +57,28 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const isAuthenticated = !!cookies.get('user'); // Check if user is authenticated
   if (to.path === '/cart' && !isAuthenticated) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Please login first',
+      showConfirmButton: false,
+      timer: 1500
+    });
+    next('/login'); // Redirect to login if not authenticated
+  }else if (to.path === '/purchase' && !isAuthenticated) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Please login first',
+      showConfirmButton: false,
+      timer: 1500
+    });
     next('/login'); // Redirect to login if not authenticated
   }else if (to.path === '/user' && !isAuthenticated) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Please login first',
+      showConfirmButton: false,
+      timer: 1500
+    });
     next('/login'); // Redirect to login if not authenticated
   } else if (to.path === '/login' && isAuthenticated) {
     next('/home'); 
